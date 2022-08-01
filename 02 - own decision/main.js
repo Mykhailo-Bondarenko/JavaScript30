f92a62013a7071075c719403ba97d465d399f142
Mykhailo Bondarenko <mikhailzyagun@gmail.com>
2022-08-01 20:59:02 +0300

    change arrows of clock in 02 lesson

--- 02 - own decision/main.js
+++ 02 - own decision/main.js
@@ -2,7 +2,6 @@ const second = document.querySelector('.second-hand');
 const minute = document.querySelector('.min-hand');
 const hour = document.querySelector('.hour-hand');
 
-console.log(second);
 function getTime() {
   const time = new Date();
 
@@ -13,6 +12,7 @@ function getTime() {
   const minutes = time.getMinutes();
   const minutesDeg = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
   minute.style.transform = `rotate(${minutesDeg}deg)`;
+
 
   const hours = time.getHours();
   const hoursDeg = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
